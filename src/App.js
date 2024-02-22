cb662319c80d71af9f2b884ffaec8ecad38cb47d
Humza Aamir <144220544+Humza911@users.noreply.github.com>
2024-02-22 13:02:42 +1100

    minor changes, project paused

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import { Component } from 'react';
+import CardList from './components/cardlist/cardlist.component';
+import SearchBox from './components/searchbox/searchbox.component';
 
 class App extends Component {
   constructor(){
@@ -9,58 +11,47 @@ class App extends Component {
       monsters: [],
       searchField:''
     };
-    console.log ('1: constructor');
   }
   componentDidMount(){
-    console.log ('3: component');
     fetch('https://jsonplaceholder.typicode.com/users')
     .then (response => response.json())
     .then (users => this.setState(
       () => {
         return {monsters:users}
       },
-      () => {
-        console.log(this.state)
-      }
     ))
     
   }    
   
-  render() {
-    console.log ('2: render');
-    const filteredMonsters = this.state.monsters.filter((monster)=>
+  onSearch = (event) =>
+  {
+    const searchInput = event.target.value.toLowerCase();            
+    this.setState(()=>
+    {
+      return {searchField:searchInput};
+    },           
+    )
+  } 
+
+  render() 
+  {
+    const {monsters, searchField}  = this.state;
+
+    const {onSearch} = this;
+    const filteredMonsters = monsters.filter((monster)=>
             {
-              return monster.name.toLowerCase().includes(this.state.searchField);
-            })
+              return monster.name.toLowerCase().includes(searchField);
+            })    
     return (
-      <div className="App">
-        <input 
-        className='search-box' 
-        type='search' 
-        placeholder='Search here' 
-        onChange={(event)=>
-          {
-            const searchInput = event.target.value.toLowerCase();            
-            this.setState(()=>
-            {
-              return {searchField:searchInput};
-            },           
-            )
-          }   
-        }
-        />
-        {
-          filteredMonsters.map((monster)=>{
-            return ( 
-            <div key={monster.id}>
-            <h1>{monster.name}</h1>
-            </div>
-            ); 
-          })
-        }
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', height: '100vh' }}>
+      
+      <SearchBox OnChangeHandler={onSearch} />
+      <div>
+        <CardList monsters={filteredMonsters} />
       </div>
-    );
-  }
+      </div>
+      );
+    }
 }
 
 export default App;
